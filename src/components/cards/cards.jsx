1135b1525d59099110121d3b62bb24b26b34fc39
toqitahmid <rubaiathali@gmail.com>
2026-04-19 21:51:11 +0600

    add toggoling between products and cart buttons

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,19 +1,47 @@
 import React from "react";
-import { use } from "react";
+import { use, useState } from "react";
 import Card from "./card";
+import Cart from "../carts/cart";
 
 const Cards = ({ cardJson }) => {
+  const [button, setButton] = useState("products");
   const cards = use(cardJson);
   return (
     <>
-      <div></div>
-      <div className="w-8/12 mx-auto grid grid-cols-3 gap-4 mb-20">
-        {cards.map((card) => (
-          <Card 
-          key={card.id} 
-          card={card}
-          ></Card>
-        ))}
+      <div className="w-8/12 mx-auto flex justify-center mt-20 mb-30">
+        <button
+          onClick={() => {
+            setButton("products");
+          }}
+          className={`btn w-35 h-12 rounded-l-3xl
+        ${button === "products" ? "bg-linear-to-bl from-violet-500 to-fuchsia-500" : "btn"}`}
+        >
+          Products
+        </button>
+        <button
+          onClick={() => {
+            setButton("cart");
+          }}
+          className={`btn w-35 h-12 rounded-r-3xl
+        ${button === "cart" ? "bg-linear-to-bl from-violet-500 to-fuchsia-500" : "btn"}`}
+        >
+          Cart (2)
+        </button>
+      </div>
+      
+
+      <div className="w-8/12 mx-auto grid grid-cols-3 gap-4 mb-10">
+        {button === "products" && 
+        cards.map((card) => (
+          <Card key={card.id} card={card}></Card>
+        ))
+        }
+      </div>
+
+      <div className="w-8/12 mx-auto">
+        {button === "cart" && (
+          <Cart></Cart>
+        )}
       </div>
     </>
   );
---
+++ src/components/carts/cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+    return(
+    <>
+    <div className="p-5 bg-base-200">
+        <h1 className='text-2xl font-bold'>Your Cart</h1>
+    </div>;
+    </>);
+};
+
+export default Cart;
